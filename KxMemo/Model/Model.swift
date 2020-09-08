18cb80be4c8d8509b3d8bc76427347aaae98e46c
zokjebi <myimgman@gmail.com>
2020-09-09 00:15:40 +0900

    2020-09-09-02 zokjebi - 메모앱 만들기 #6 (완강)

---
+++ KxMemo/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  KxMemo
+//
+//  Created by MAC on 2020/09/09.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe + 😀 = 😡")
+    ]
+}
