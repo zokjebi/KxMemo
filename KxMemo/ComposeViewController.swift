e25e4dcf1057ba0954a0306ec83b3576e4f42aa2
zokjebi <myimgman@gmail.com>
2020-09-09 23:42:38 +0900

    2020-09-09-05 zokjebi - 메모앱 만들기 #10 (완강)

---
+++ KxMemo/ComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeViewController.swift
+//  KxMemo
+//
+//  Created by MAC on 2020/09/09.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
