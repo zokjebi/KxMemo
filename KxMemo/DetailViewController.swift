2a936a99c6781069bcdbe639b1ee9ba1d7b6a228
zokjebi <myimgman@gmail.com>
2020-09-11 01:10:46 +0900

    2020-09-11-02 zokjebi - 메모앱 만들기 #15 (완강)

--- KxMemo/DetailViewController.swift
+++ KxMemo/DetailViewController.swift
@@ -28,3 +28,27 @@ class DetailViewController: UIViewController {
     */
 
 }
+
+extension DetailViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+            
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+            
+            return cell
+        default:
+            fatalError()
+            
+        }
+    }
+    
+    
+}
